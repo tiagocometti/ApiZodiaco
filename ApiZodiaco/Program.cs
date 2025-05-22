ba0934a0caf2432a4a11bb142282ebe83998d8b3
tiagocometti <tiagocometti186@gmail.com>
2025-05-22 09:42:29 -0300

    teste program

--- ApiZodiaco/Program.cs
+++ ApiZodiaco/Program.cs
@@ -1,11 +1,12 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using AstrologiaAPI.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Porta dinâmica para o Render (usa $PORT ou 8080 localmente)
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    serverOptions.ListenAnyIP(8080);
+    serverOptions.ListenAnyIP(int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "8080"));
 });
 
 builder.Services.AddControllers();
@@ -21,17 +22,13 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();  // Mantenha Swagger mesmo em produção para testes
 
-app.UseHttpsRedirection();
+// Remova UseHttpsRedirection() se não configurar HTTPS manualmente
+// app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
